f2193109a6fe362a046a8985aa2e31fbd93ce673
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-06-27 21:42:23 +0300

    last modifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import Container from 'react-bootstrap/Container';
+import Container from 'react-bootstrap/Container'; 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Collapse from 'react-bootstrap/Collapse';
